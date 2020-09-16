2ef4a8bca645412cacea47d29d87b42eff8dd6e9
Tyler Young <tyoung900@gmail.com>
2020-09-16 22:32:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,11 @@ katz_deli = []
 def line(deli)
   if deli.empty
     puts "The line is currently empty."
-  elsif katz_deli.length > 0
-    return katz_deli.length
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
 end
